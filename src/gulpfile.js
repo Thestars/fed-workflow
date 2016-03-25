05c07afb748ecaaa3a71dba8844547c231091c91
Ziv <zuoww@lotuseed.com>
2016-03-25 16:40:11 +0800

    add auto refresh for browser

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -9,6 +9,7 @@ var uglify = require('gulp-uglify');
 var useref = require('gulp-useref');
 var sass = require('gulp-sass');
 var clean = require('gulp-clean');
+var livereload = require('gulp-livereload'); // Auto refresh browser.
 
 var paths = {
     html: [
@@ -36,34 +37,39 @@ var dist = "../dist"; // dist目录
  */
 gulp.task('images', function() {
     gulp.src(paths.images)
-        .pipe(gulp.dest(output + "/images"));
+        .pipe(gulp.dest(output + "/images"))
+        .pipe(livereload());
 });
 
 gulp.task('html', function() {
     gulp.src(paths.html)
         .pipe(fileInclude())
-        .pipe(gulp.dest(output));
+        .pipe(gulp.dest(output))
+        .pipe(livereload());
 });
 
 gulp.task('js', function() {
     gulp.src(paths.js)
-        .pipe(gulp.dest(output + "/js"));
+        .pipe(gulp.dest(output + "/js"))
+        .pipe(livereload());
 });
 
 gulp.task('font', function() {
     gulp.src(paths.font)
-        .pipe(gulp.dest(output + "/css/fonts"));
+        .pipe(gulp.dest(output + "/css/fonts"))
+        .pipe(livereload());
 });
 
 gulp.task('sass', function() {
    return gulp.src(paths.sass)
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(output + '/css'));
+    .pipe(gulp.dest(output + '/css'))
+    .pipe(livereload());
 });
 
 // =============压缩合并build资源============== //
 gulp.task('dist', ['run.dist'], function() {
-
+    
     gulp.src(dist + "/*")
         .pipe(clean({force: true}));
 
@@ -88,6 +94,7 @@ gulp.task('dist', ['run.dist'], function() {
 });
 
 gulp.task('run.dist', function() {
+    
     exec("node ../app.js /dist", function(err, stdout, stderr) {
         console.log(stdout);
         if (err) console.log("start server error:" + err);
@@ -103,6 +110,7 @@ gulp.task('run.build', function() {
 
 // 默认构建
 gulp.task('default', ['images', 'sass', 'html', 'js', 'font', 'run.build'], function() {
+    livereload.listen();
     gulp.watch(paths.sass, ['sass']);
     gulp.watch(paths.html, ['html']);
     gulp.watch(paths.images, ['images']);
